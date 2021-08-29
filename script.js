8756675cb00e5a2692cc38350f5736439c89e7a6
experiencei <ayelojahighbee01@gmail.com>
2021-08-29 22:27:18 +0100

    rebuiding up the UI with drag element

--- script.js
+++ script.js
@@ -88,8 +88,31 @@ function updateDOM() {
     createItemEl(onHoldListEl, 3, onHoldItem, index);
   });
 
+// Don't run more than once, Update Local Storage
+updatedOnLoad = true;
+updateSavedColumns();
 }
 
+// Allows arrays to reflect Drag and Drop items
+function rebuildArrays() {
+  backlogListArray = [];
+  for (let i = 0; i < backlogListEl.children.length; i++) {
+    backlogListArray.push(backlogListEl.children[i].textContent);
+  }
+  progressListArray = [];
+  for (let i = 0; i < progressListEl.children.length; i++) {
+    progressListArray.push(progressListEl.children[i].textContent);
+  }
+  completeListArray = [];
+  for (let i = 0; i < completeListEl.children.length; i++) {
+    completeListArray.push(completeListEl.children[i].textContent);
+  }
+  onHoldListArray = [];
+  for (let i = 0; i < onHoldListEl.children.length; i++) {
+    onHoldListArray.push(onHoldListEl.children[i].textContent);
+  }
+  updateDOM();
+}
 // When Item Enters Column Area
 function dragEnter(column) {
   listColumns[column].classList.add('over');
@@ -123,5 +146,4 @@ function drop(e) {
 }
 
 // On Load
-updateDOM();
 updateDOM();
